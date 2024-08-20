83fdaaab44dec1678b479a7d317470488d291dae
sudharanik861 <165420713+sudharanik861@users.noreply.github.com>
2024-08-20 18:01:42 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# Variables.tf 
+variable "admin_username" {
+  description = "The admin username for the VM"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "The admin password for the VM"
+  type        = string
+  sensitive   = true
+}
